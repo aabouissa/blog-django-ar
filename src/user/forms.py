168e8cb040af70cd168e84031027dd15af5f0a40
Ahmad Abouissa <abouissa.ahmad@gmail.com>
2019-05-02 23:29:59 +0300

    Activate the registration form

---
+++ src/user/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    username = forms.CharField(label='اسم المستخدم', max_length=30,
+                               help_text='اسم المستخدم يجب ألا يحتوي على مسافات.')
+    email = forms.EmailField(label='البريد الإلكتروني')
+    first_name = forms.CharField(label='الاسم الأول')
+    last_name = forms.CharField(label='الاسم الأخير')
+    password1 = forms.CharField(
+        label='كلمة المرور', widget=forms.PasswordInput(), min_length=8)
+    password2 = forms.CharField(
+        label='تأكيد كلمة المرور', widget=forms.PasswordInput(), min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'password1', 'password2')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('كلمة المرور غير متطابقة')
+        return cd['password2']
+
+    def clean_username(self):
+        cd = self.cleaned_data
+        if User.objects.filter(username=cd['username']).exists():
+            raise forms.ValidationError('يوجد مستخدم مسجل بهذا الاسم.')
+        return cd['username']
--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            # messages.success(
+            #    request, 'تهانينا {} لقد تمت عملية التسجيل بنجاح.'.format(username))
+            messages.success(
+                request, f'تهانينا {username} لقد تمت عملية التسجيل بنجاح.')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html', {
+        'title': 'التسجيل',
+        'form': form,
+    })
