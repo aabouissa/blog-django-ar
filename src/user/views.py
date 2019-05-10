e9f15c8dc0edbefff73be47a8fadab76b09dd336
Ahmad Abouissa <abouissa.ahmad@gmail.com>
2019-05-10 23:32:16 +0300

    Edit login form

--- src/user/views.py
+++ src/user/views.py
@@ -25,8 +25,8 @@ def register(request):
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
